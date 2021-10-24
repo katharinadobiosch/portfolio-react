d16679a12bca50aed4a2bfa39b83b012157c6d57
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2021-10-24 16:23:55 +0200

    changed design

--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -2,28 +2,50 @@ import React, { useState } from "react";
 import styles from "../styles/_hamburgerMenu.module.scss";
 
 // import { HashLink as Link } from "react-router-hash-link";
-import { NavLink, Link, animateScroll as scroll } from "react-scroll";
+// import { NavLink, Link, animateScroll as scroll } from "react-scroll";
 
-const HamburgerMenu = (props) => {
+const HamburgerMenu = () => {
+    const [showMenu, setShowMenu] = useState(false);
+
+    const closeMenu = (event) => {
+        event.preventDefault();
+        setShowMenu(false);
+        console.log("button was clicked");
+    };
+
+    // if (value === true) return false;
+
+    // // MENU IS VISIBLE WHEN WEBSITE LOADS
+    // const [showMenu, setShowMenu] = useState(true);
+
+    // const closeMenu = (event) => {
+    //     event.preventDefault();
+    //     setShowMenu(false);
+    //     console.log("button was clicked");
+    // };
+
+    // const closeMenu = (event) => {
+    //     if (event.target.value === true) {
+    //         setShowMenu(false);
+    //         console.log("button was clicked");
+    //     }
+    // };
+
     //     if (!props.show) {
     //         return null;
     //     }
-
-    //     // const closeHamburger = () => {
-    //     //     const [isOpen, setIsOpen] = useState(false);
-    //     // };
-
-    //     const closeMenu = () => {
-    //         const [isOpen, setIsOpen] = useState(false);
-
-    //         setIsOpen(false);
-    // };
 
     return (
         <>
             <nav role="navigation">
                 <div className={styles.menuToggle}>
-                    <input className={styles.input} type="checkbox" />
+                    <input
+                        className={styles.input}
+                        type="checkbox"
+                        // onChange={closeMenu}
+                        // // MENU IS VISIBLE WHEN WEBSITE LOADS
+                        // checked={showMenu}
+                    />
 
                     {/* Some spans to act as a hamburger.*/}
 
@@ -33,19 +55,31 @@ const HamburgerMenu = (props) => {
 
                     <ul className={`${styles.menu} ${styles.ul}`}>
                         <a className={styles.a} href="#about">
-                            <li className={styles.li}>::about</li>
+                            <li
+                                className={styles.li}
+                                onClick={closeMenu}
+                                // value={true}
+                            >
+                                ::about
+                            </li>
                         </a>
 
                         <a className={styles.a} href="#skills">
-                            <li className={styles.li}>::skills</li>
+                            <li className={styles.li} onClick={closeMenu}>
+                                ::skills
+                            </li>
                         </a>
 
                         <a className={styles.a} href="#projects">
-                            <li className={styles.li}>::projects</li>
+                            <li className={styles.li} onClick={closeMenu}>
+                                ::projects
+                            </li>
                         </a>
 
                         <a className={styles.a} href="#contact">
-                            <li className={styles.li}>::contact</li>
+                            <li className={styles.li} onClick={closeMenu}>
+                                ::contact
+                            </li>
                         </a>
 
                         <a
@@ -54,7 +88,7 @@ const HamburgerMenu = (props) => {
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <li className={styles.icons}>
+                            <li className={styles.icons} onClick={closeMenu}>
                                 <i class="fab fa-github"></i>
                             </li>
                         </a>
@@ -65,7 +99,7 @@ const HamburgerMenu = (props) => {
                             target="_blank"
                             rel="noopener noreferrer"
                         >
-                            <li className={styles.icons}>
+                            <li className={styles.icons} onClick={closeMenu}>
                                 <i class="fab fa-linkedin"></i>
                             </li>
                         </a>
