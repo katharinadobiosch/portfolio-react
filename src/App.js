921f6e3cebb35df774168f6bf13172b05c3900d1
Kathi <67746234+katharinadobiosch@users.noreply.github.com>
2021-11-02 10:30:23 +0100

    added smooth scrolling

--- src/App.js
+++ src/App.js
@@ -1,45 +1,23 @@
 import React from "react";
-import {
-    Router,
-    Route,
-    // Redirect,
-    Switch,
-} from "react-router-dom";
-import { createBrowserHistory } from "history";
-
 import "./styles/app.module.scss";
-
 import LoadingBar from "./components/LoadingBar";
 import Header from "./components/Header";
-// import Hello from "./components/Hello";
 import Welcome from "./components/Welcome";
 import About from "./components/About";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
-// import HamburgerMenu from "./components/HamburgerMenu";
-import HomeComponent from "./components/HomeComponent";
-
-export const customHistory = createBrowserHistory();
 
 const App = () => {
     return (
         <div>
-            <Router history={customHistory}>
-                <Switch>
-                    {/* <LoadingBar /> */}
-                    {/* <Header /> */}
-                    {/* <About /> */}
-                    {/* <Skills /> */}
-                    {/* <Projects /> */}
-                    {/* <Contact />  */}
-                    <Route path="/" component={Welcome} />
-                    {/* <Route path="/about" component={About} /> */}
-                    {/* <Route path="/skills" component={Skills} /> */}
-                    {/* <Route path="/projects" component={Projects} /> */}
-                    {/* <Route path="/contact" component={Contact} /> */}
-                </Switch>
-            </Router>
+            <LoadingBar />
+            <Header />
+            <Welcome />
+            <About />
+            <Skills />
+            <Projects />
+            <Contact />
         </div>
     );
 };
--- src/components/HamburgerMenu.js
+++ src/components/HamburgerMenu.js
@@ -1,104 +1,74 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-
+import React from "react";
 import styles from "../styles/_hamburgerMenu.module.scss";
 
 // import { HashLink as Link } from "react-router-hash-link";
 // import { NavLink, Link, animateScroll as scroll } from "react-scroll";
 
 const HamburgerMenu = () => {
+  const onRouteHandler = (id) => {
+    //  window.location.replace("/" + path);
+    const element = document.getElementById(id);
+    element.scrollIntoView({ behavior: "smooth" });
+  };
 
-    const closeMenu = (event) => {
-        event.preventDefault();
-        window.location.reload();
-        console.log("menu item was clicked!");
-    };
+  return (
+    <>
+      <nav role="navigation">
+        <div className={styles.menuToggle}>
+          <input className={styles.input} type="checkbox" />
 
-  
+          <span className={styles.span}></span>
+          <span className={styles.span}></span>
+          <span className={styles.span}></span>
 
-    return (
-        <>
-            <nav role="navigation">
-                <div className={styles.menuToggle}>
-                    <input
-                        className={styles.input}
-                        type="checkbox"
-                        // onChange={closeMenu}
-                        // // MENU IS VISIBLE WHEN WEBSITE LOADS
-                        // checked={showMenu}
-                    />
+          <ul className={`${styles.menu} ${styles.ul}`}>
+            <span className={styles.a} onClick={() => onRouteHandler("about")}>
+              <li className={styles.li}>::about</li>
+            </span>
 
-                    {/* Some spans to act as a hamburger.*/}
+            <span className={styles.a} onClick={() => onRouteHandler("skills")}>
+              <li className={styles.li}>::skills</li>
+            </span>
 
-                    <span className={styles.span}></span>
-                    <span className={styles.span}></span>
-                    <span className={styles.span}></span>
+            <span
+              className={styles.a}
+              onClick={() => onRouteHandler("projects")}
+            >
+              <li className={styles.li}>::projects</li>
+            </span>
 
-                    <ul className={`${styles.menu} ${styles.ul}`}>
-                        <a className={styles.a} href="/about">
-                            <li
-                                className={styles.li}
-                                onClick={closeMenu}
-                                // value={true}
-                            >
-                                ::about
-                            </li>
-                        </a>
+            <span
+              className={styles.a}
+              onClick={() => onRouteHandler("contact")}
+            >
+              <li className={styles.li}>::contact</li>
+            </span>
 
-                        {/* <Link to="/about" className={styles.a}>
-                            <li
-                                className={styles.li}
-                                onClick={closeMenu}
-                                // value={true}
-                            >
-                                ::about
-                            </li>
-                        </Link> */}
+            <a
+              className={styles.a}
+              href="https://github.com/katharinadobiosch"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li className={styles.icons}>
+                <i class="fab fa-github"></i>
+              </li>
+            </a>
 
-                        <a className={styles.a} href="#skills">
-                            <li className={styles.li} onClick={closeMenu}>
-                                ::skills
-                            </li>
-                        </a>
-
-                        <a className={styles.a} href="#projects">
-                            <li className={styles.li} onClick={closeMenu}>
-                                ::projects
-                            </li>
-                        </a>
-
-                        <a className={styles.a} href="#contact">
-                            <li className={styles.li} onClick={closeMenu}>
-                                ::contact
-                            </li>
-                        </a>
-
-                        <a
-                            className={styles.a}
-                            href="https://github.com/katharinadobiosch"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <li className={styles.icons} onClick={closeMenu}>
-                                <i class="fab fa-github"></i>
-                            </li>
-                        </a>
-
-                        <a
-                            // className={styles.a}
-                            href="https://www.linkedin.com/in/katharina-dobiosch/"
-                            target="_blank"
-                            rel="noopener noreferrer"
-                        >
-                            <li className={styles.icons} onClick={closeMenu}>
-                                <i class="fab fa-linkedin"></i>
-                            </li>
-                        </a>
-                    </ul>
-                </div>
-            </nav>
-        </>
-    );
+            <a
+              href="https://www.linkedin.com/in/katharina-dobiosch/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <li className={styles.icons}>
+                <i class="fab fa-linkedin"></i>
+              </li>
+            </a>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
 };
 
 export default HamburgerMenu;
